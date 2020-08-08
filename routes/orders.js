e815322105c698ac7ea5b017aa6a0ace83ce6aac
Melakeselam Moges <melakeselammoges@gmail.com>
2020-08-08 18:31:23 -0500

    Completed router that finds order history it is filtered by farmer id and status

--- routes/orders.js
+++ routes/orders.js
@@ -3,8 +3,9 @@ const orderController = require('../controllers/orderController')
 const express = require('express');
 const router = express.Router();
 
-//this route will be accessed at the time of checkout
-router.get('/',orderController.save);
+//this route will be accessed by farmer to get all order history of specific 
+//status
+router.get('/:status/:farmerId',orderController.getAllOrdersOfFarmer);
 
 //this route will be accessed at the time of checkout
 router.post('/',orderController.save);
