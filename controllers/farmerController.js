7bbd0521c68fc6a6077f1034c0fa107fcd5a7810
MichealSebre <msebre@gmail.com>
2020-08-08 18:16:44 -0500

    Farmer full path for registration

--- controllers/farmerController.js
+++ controllers/farmerController.js
@@ -1,1 +1,17 @@
-//COntroller for farmer 
+const farmerService = require('../services/farmerServices');
+const farmerMOdel = require ('../models/farmer');
+
+module.exports.farmerRegistration = async(req, res , next) => {
+    const farmer=req.body;
+
+    try {
+          const addFaremerResult = await farmerService.registerFarmer(farmer)
+            console.log(addFaremerResult)
+          if(addFaremerResult.data) {
+            addFaremerResult.satus=200;
+            res.status(200).json(addFaremerResult);
+          }else{
+              next(addFaremerResult.error);
+          }
+    }  catch (e) { }
+}
