cb2b8e1d8ddf34a2be1c873e1c09eb1c5c5808a8
Mohammad Zahedy <mohammad.zahedy@gmail.com>
2020-08-09 11:47:27 -0500

    Farmer routes adding middleware encrypt and authRe

--- routes/farmers.js
+++ routes/farmers.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const farmerController = require('../controllers/farmerController');
-const { Farmer } = require('../models/farmer');
-const { Customer } = require('../models/customer');
-const { Order } = require('../models/order');
+const password = require('../middlewares/password');
+const { authRequest } = require('../middlewares/authRequest');
 
-router.post('/signup',farmerController.farmerRegistration);
+router.post('/signup',password.encrypt, farmerController.farmerRegistration);
 
 
 //router for the farmer to be signed in 
 router.post('/signin',farmerController.farmerSignIn);
  
 //router for adding new Products 
-router.post('/:id/addproduct',farmerController.addProducts);
+router.post('/:id/addproduct',authRequest ,farmerController.addProducts);
 
 //router to fetch products from farmer product list 
-router.get('/:id/fetch',farmerController.getProducts);
-
-
+router.get('/:id/fetch' ,authRequest ,farmerController.getProducts);
 
 module.exports = router;
