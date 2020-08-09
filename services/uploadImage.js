540eedd10c7d3271a28360e7ce3b506b37f671cf
Mohammad Zahedy <mohammad.zahedy@gmail.com>
2020-08-08 22:38:13 -0500

    add comments on uploadimage and authRequest

--- services/uploadImage.js
+++ services/uploadImage.js
@@ -10,6 +10,7 @@ aws.config.update({
 });
 const s3 = new aws.S3();
  
+// Filter all files except jpeg and png
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
       cb(null, true)
@@ -28,6 +29,7 @@ const upload = multer({
       cb(null, {fieldName: file.fieldname});
     },
     key: function (req, file, cb) {
+      // get the extension for image
       const ext = file.originalname.split('.');
       cb(null, Date.now().toString()+"."+ext[1]);
     }
